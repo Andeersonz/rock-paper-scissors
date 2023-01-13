be7971af26ba74124cbb76a21ed55904e8d03b40
Andeerson <michaelangelomalana@gmail.com>
2023-01-12 21:39:40 -0500

    Finished coding ROCK algorithm

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+let list = ['Rock', 'Paper', 'Scissors'];
+
 function getComputerChoice() {
-  let list = ['Rock', 'Paper', 'Scissors'];
   let randomizer = list[Math.floor(Math.random() * list.length)];
 
   return randomizer;
 }
 
+function playRound(playerSelection, computerSelection) {
+  
+  /*if (playerSelection.toLowerCase() == list[0].toLowerCase() && computerSelection == list[1].toLowerCase())
+    console.log("You lose! Paper beats Rock!");
+  else if (playerSelection.toLowerCase() == list[0].toLowerCase() && computerSelection == list[2].toLowerCase())
+    console.log("You win! Rock beats Scissors!");
+  else if ()*/
 
+  switch (playerSelection.toLowerCase()) {
+    case list[0].toLowerCase():
+      if (computerSelection.toLowerCase() == list[1].toLowerCase())
+        return "You lose! " + computerSelection.toUpperCase() + " beats " + playerSelection.toUpperCase() + "!";
+      else if (computerSelection.toLowerCase() == list[2].toLowerCase())
+        return "You win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase() + "!";
+  }
+}
 
-console.log(getComputerChoice())
+function game(playRound) {
+
+}
+
+const playerSelection = "rock"
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
